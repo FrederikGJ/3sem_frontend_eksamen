a73be1e16550e98e8a471008f7ad0467e8303fef
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2023-12-18 13:32:49 +0100

    Refactor Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,23 @@
-import  { useEffect, useState } from 'react';
-import facade from '../facades/loginFacade';
+import { useEffect, useState } from "react";
+import facade from "../facades/loginFacade";
 
 export const Login = () => {
-    const init = { username: '', password: '' };
-    const [loginCredentials, setLoginCredentials] = useState(init);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [dataFromServer, setDataFromServer] = useState('Loading...');
+  const init = { username: "", password: "" };
+  const [loginCredentials, setLoginCredentials] = useState(init);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [dataFromServer, setDataFromServer] = useState("Loading...");
 
-    useEffect(() => {
-        facade.fetchData().then((data) => setDataFromServer(data));
-    }, [isLoggedIn]);
-
+  useEffect(() => {
+    facade.fetchData().then((data) => setDataFromServer(data));
+  }, [isLoggedIn]);
 
   const performLogin = (evt) => {
     evt.preventDefault();
-    facade.login(loginCredentials.username, loginCredentials.password, setIsLoggedIn);
+    facade.login(
+      loginCredentials.username,
+      loginCredentials.password,
+      setIsLoggedIn
+    );
   };
 
   const onChange = (evt) => {
@@ -28,30 +31,33 @@ export const Login = () => {
     <>
       <div>
         <h1>
-          Login Demo 
-
+          Login Demo
           <form onChange={onChange}>
             <input placeholder="User Name" id="username" />
             <input placeholder="Password" id="password" />
             <button onClick={performLogin}>Login</button>
-      </form>
-      <div>
-        {isLoggedIn ? (
-        <div> 
-        <p>Du er logget ind, {facade.getUserRoles()}</p>
-        <button onClick={() => facade.logout(setIsLoggedIn)}> LogOut</button>
+          </form>
+          <div>
+            {isLoggedIn ? (
+              <div>
+                <p>Du er logget ind, {facade.getUserRoles()}</p>
+                <button onClick={() => facade.logout(setIsLoggedIn)}>
+                  {" "}
+                  LogOut
+                </button>
 
-        {dataFromServer.map((users) => (
-        <p key ={users.id}>{users.username}</p>))}
-
-        </div>
-        ) : (
-        <p>Log på for at være med</p>)}
-      </div>
+                {dataFromServer.map((users) => (
+                  <p key={users.id}>{users.username}</p>
+                ))}
+              </div>
+            ) : (
+              <p>Log på for at være med</p>
+            )}
+          </div>
         </h1>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
